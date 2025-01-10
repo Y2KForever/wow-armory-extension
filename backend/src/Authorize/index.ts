94984b0c457d3b585077370ec95a1e71cd4916b8
Y2KForever <niklas.hansen1@gmail.com>
2025-01-10 18:07:33 +0100

    minor fixes to auth/callback

--- backend/src/Authorize/index.ts
+++ backend/src/Authorize/index.ts
@@ -1,11 +1,24 @@
+import { DynamoDBClient, UpdateItemCommand } from '@aws-sdk/client-dynamodb';
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
 import { ApiResult, generateRandomState } from '../utils/utils';
 import { middyCore } from '../utils/middyWrapper';
 import { getClientCredentials } from '../utils/secretsManager';
 
+const ddbClient = new DynamoDBClient({});
+
 const lambdaHandler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
+  const headers = event.headers;
+  let xUserId = headers['x-user-id'];
+  if (process.env['NODE_ENV'] !== 'development') {
+    if (!xUserId) {
+      return ApiResult(400, JSON.stringify({ error: 'No user id' }));
+    }
+  } else {
+    xUserId = '72606078';
+  }
+
   const baseUrl = process.env['OAUTH_BASE_URL'];
-  const redirectUri = process.env['REDIRECT_URL'];
+  const redirectUri = process.env['REDIRECT_URI'];
   if (!baseUrl) {
     return ApiResult(500, JSON.stringify({ error: 'Server error. Try again later' }));
   }
@@ -16,14 +29,45 @@ const lambdaHandler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayP
 
   const clientCredentialsSecret = await getClientCredentials();
   const state = generateRandomState();
-  const redirectUrl = `https://eu.${baseUrl}/authorize?response_type=code&client_id=${clientCredentialsSecret.client_id}&redirect_uri=${redirectUri}&state=${state}`;
+
+  const now = new Date().toISOString();
+
+  try {
+    const createParams = new UpdateItemCommand({
+      TableName: 'wow-extension-profiles',
+      Key: {
+        user_id: { S: xUserId },
+      },
+      UpdateExpression: `
+          SET
+          #st = :st,
+          updated_at = :updated_at,
+          ${'created_at = if_not_exists(created_at, :created_at)'}
+     `,
+      ExpressionAttributeNames: {
+        '#st': 'state',
+      },
+      ExpressionAttributeValues: {
+        ':st': { S: state },
+        ':updated_at': { S: now },
+        ':created_at': { S: now },
+      },
+    });
+
+    await ddbClient.send(createParams);
+  } catch (err) {
+    console.log(`Error trying to create user. UserId: ${xUserId}`);
+    throw new Error(err as string);
+  }
+
+  const redirectUrl = `https://eu.${baseUrl}/authorize?response_type=code&client_id=${clientCredentialsSecret.client_id}&redirect_uri=${redirectUri}&state=${state}&scope=wow.profile`;
 
   return {
     statusCode: 302,
     headers: {
       Location: redirectUrl,
     },
-    body: "",
+    body: '',
   };
 };
 
--- backend/src/Callback/index.ts
+++ backend/src/Callback/index.ts
@@ -1,15 +1,36 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
-import { ApiResult } from '../utils/utils';
+import { ApiResult, simplifyDynamoDBResponse } from '../utils/utils';
 import { middyCore } from '../utils/middyWrapper';
 import { getClientCredentials } from '../utils/secretsManager';
 import { TokenResponse } from '../types/battleNet';
+import { DynamoDBClient, GetItemCommand, UpdateItemCommand } from '@aws-sdk/client-dynamodb';
+
+const ddbClient = new DynamoDBClient({});
 
 const lambdaHandler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
   const baseUrl = process.env['OAUTH_BASE_URL'];
+  const redirectUri = process.env['REDIRECT_URI'];
+
+  const headers = event.headers;
+  let xUserId = headers['x-user-id'];
+  if (process.env['NODE_ENV'] !== 'development') {
+    if (!xUserId) {
+      return ApiResult(400, JSON.stringify({ error: 'No user id' }));
+    }
+  } else {
+    xUserId = '72606078';
+  }
+
   const clientCredentialsSecret = await getClientCredentials();
+
   if (!baseUrl) {
     return ApiResult(500, JSON.stringify({ error: 'Server error. Try again later' }));
   }
+
+  if (!redirectUri) {
+    return ApiResult(500, JSON.stringify({ error: 'Server error. Try again later' }));
+  }
+
   const code = event.queryStringParameters?.code;
   const state = event.queryStringParameters?.state;
 
@@ -17,31 +38,76 @@ const lambdaHandler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayP
     return {
       statusCode: 400,
       body: JSON.stringify({
-        error: 'Something went wrong',
+        error: 'Something went wrong. Please try again later.',
       }),
     };
   }
 
+  const getCommand = new GetItemCommand({
+    TableName: 'wow-extension-profiles',
+    Key: {
+      user_id: { S: xUserId },
+    },
+  });
+
+  const { Item } = await ddbClient.send(getCommand);
+
+  const simplifiedResponse = simplifyDynamoDBResponse(Item);
+
+  if (simplifiedResponse.state !== state) {
+    console.log('db state', simplifiedResponse.state);
+    console.log('request state', state);
+    return ApiResult(500, JSON.stringify({ error: 'Server error. Please try again later.' }));
+  }
+
+  const b64 = Buffer.from(`${clientCredentialsSecret.client_id}:${clientCredentialsSecret.client_secret}`).toString(
+    'base64',
+  );
+
   try {
-    const tokenResponse = await fetch(
-      `https://eu.${baseUrl}?grant_type=authorization_code&code=${code}&redirect_uri=http://localhost&client_id=${clientCredentialsSecret.client_id}`,
-      {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-        },
+    const tokenResponse = await fetch(`https://${baseUrl}/token`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${b64}`,
       },
-    );
+      body: `redirect_uri=${redirectUri}&grant_type=authorization_code&code=${code}`,
+    });
 
     if (!tokenResponse.ok) {
-      return ApiResult(500, JSON.stringify({ error: 'something went wrong' }));
+      return ApiResult(500, JSON.stringify({ error: 'Something went wrong. Please try again later.' }));
     }
 
     const respJSON = (await tokenResponse.json()) as TokenResponse;
 
-    console.log('respJson', JSON.stringify(respJSON));
+    const now = new Date().toISOString();
 
-    return ApiResult(200, JSON.stringify(''));
+    const updateItemParams = new UpdateItemCommand({
+      TableName: 'wow-extension-profiles',
+      Key: {
+        user_id: { S: xUserId },
+      },
+      UpdateExpression: `
+              SET
+              #st = :st,
+              updated_at = :updated_at,
+              expires_in = :expires_in,
+              ${'created_at = if_not_exists(created_at, :created_at)'}
+         `,
+      ExpressionAttributeNames: {
+        '#st': 'state',
+      },
+      ExpressionAttributeValues: {
+        ':st': { S: respJSON.access_token },
+        ':expires_in': { N: respJSON.expires_in.toString() },
+        ':updated_at': { S: now },
+        ':created_at': { S: now },
+      },
+    });
+
+    await ddbClient.send(updateItemParams);
+
+    return ApiResult(200, JSON.stringify({ status: 'success' }));
   } catch (err) {
     console.log(err);
     throw new Error(err as string);
